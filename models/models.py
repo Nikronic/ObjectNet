d51dcc5bfc6cbba92b8c07625bc160f816233530
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-12-11 02:24:45 +0330

    add modelbuilder class
    Took 56 seconds

--- models/models.py
+++ models/models.py
@@ -5,6 +5,87 @@ from . import resnet
 from lib.nn import SynchronizedBatchNorm2d
 
 
+class SegmentationModule(nn.Module):
+    def __init__(self, net_enc, net_dec, crit, deep_sup_scale=None):
+        super(SegmentationModule, self).__init__()
+        self.encoder = net_enc
+        self.decoder = net_dec
+        self.crit = crit
+        self.deep_sup_scale = deep_sup_scale
+
+    def forward(self, feed_dict, *, segSize=None):
+        # training
+        if segSize is None:
+            if self.deep_sup_scale is not None:  # use deep supervision technique
+                (pred, pred_deepsup) = self.decoder(self.encoder(feed_dict['img_data'], return_feature_maps=True))
+            else:
+                pred = self.decoder(self.encoder(feed_dict['img_data'], return_feature_maps=True))
+
+            loss = self.crit(pred, feed_dict['seg_label'])
+            if self.deep_sup_scale is not None:
+                loss_deepsup = self.crit(pred_deepsup, feed_dict['seg_label'])
+                loss = loss + loss_deepsup * self.deep_sup_scale
+
+            acc = self.pixel_acc(pred, feed_dict['seg_label'])
+            return loss, acc
+        # inference
+        else:
+            pred = self.decoder(self.encoder(feed_dict['img_data'], return_feature_maps=True), segSize=segSize)
+            return pred
+
+    def pixel_acc(self, pred, label):
+        _, preds = torch.max(pred, dim=1)
+        valid = (label >= 0).long()
+        acc_sum = torch.sum(valid * (preds == label).long())
+        pixel_sum = torch.sum(valid)
+        acc = acc_sum.float() / (pixel_sum.float() + 1e-10)
+        return acc
+
+
+class ModelBuilder():
+    # custom weights initialization
+    def weights_init(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            nn.init.kaiming_normal_(m.weight.data)
+        elif classname.find('BatchNorm') != -1:
+            m.weight.data.fill_(1.)
+            m.bias.data.fill_(1e-4)
+
+    def build_encoder(self, arch='resnet101dilated', weights=''):
+        pretrained = True if len(weights) == 0 else False
+        arch = arch.lower()
+
+        if arch == 'resnet101dilated':
+            orig_resnet = resnet.__dict__['resnet101'](pretrained=pretrained)
+            net_encoder = ResnetDilated(orig_resnet, dilate_scale=8)
+        else:
+            raise Exception('Architecture undefined!')
+
+        if len(weights) > 0:
+            print('Loading weights for net_encoder')
+            net_encoder.load_state_dict(
+                torch.load(weights, map_location=lambda storage, loc: storage), strict=False)
+        return net_encoder
+
+    def build_decoder(self, arch='ppm_deepsup', fc_dim=512, num_class=150, weights='', use_softmax=False):
+        arch = arch.lower()
+        if arch == 'ppm_deepsup':
+            net_decoder = PPMDeepsup(
+                num_class=num_class,
+                fc_dim=fc_dim,
+                use_softmax=use_softmax)
+        else:
+            raise Exception('Architecture undefined!')
+
+        net_decoder.apply(self.weights_init)
+        if len(weights) > 0:
+            print('Loading weights for net_decoder')
+            net_decoder.load_state_dict(
+                torch.load(weights, map_location=lambda storage, loc: storage), strict=False)
+        return net_decoder
+
+
 def conv3x3_bn_relu(in_planes, out_planes, stride=1):
     return nn.Sequential(
         nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False),
